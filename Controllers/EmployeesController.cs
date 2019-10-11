697cbd6d2ce21e6b8959afbb39d0ebef92efb7ac
Gunasekaran Marati Saravanan <gmaratisaravanan@bdo.ca>
2019-10-11 10:41:12 -0400

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPIDemo.Controllers
+{
+    [Authorize]
+    [EnableCors("*","*","*")]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeBDEntities entities = new EmployeeBDEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (EmployeeBDEntities entities = new EmployeeBDEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
